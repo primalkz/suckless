7266b651883b59dc87f2a4e3c89e13367bedc83c
primalkaze <44187697+primalkaze@users.noreply.github.com>
2021-06-17 10:13:44 +0530

    small change

--- dmenu/config.h
+++ dmenu/config.h
@@ -5,22 +5,22 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int centered = 0;                    /* -c option; centers dmenu on screen */
 static int min_width = 500;                    /* minimum width when centered */
 /* -h option; minimum height of a menu line */
-static unsigned int lineheight = 0;
+static unsigned int lineheight = 8;
 static unsigned int min_lineheight = 8;
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Iosevka:size=11"
+	"CaskaydiaCove Nerd Font:size=11"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#fff", "#222222" },
-	[SchemeSel] = { "#fff", "#caa9fa" },
-	[SchemeOut] = { "#000000", "#caa9fa" },
-	[SchemeMid] = { "#fff", "#61afef" },
-	[SchemeSelHighlight] = { "#ff0", "#caa9fa" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
-	[SchemeOutHighlight] = { "#ffc978", "#caa9fa" },
+	[SchemeSel] = { "#fff", "#98b379" },
+	[SchemeOut] = { "#000000", "#282c34" },
+	[SchemeMid] = { "#fff", "#282c34" },
+	[SchemeSelHighlight] = { "#000000", "#98c379" },
+	[SchemeNormHighlight] = { "#000000", "#98c379" },
+	[SchemeOutHighlight] = { "#000000", "#282c34" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
