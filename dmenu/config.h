1b11dac4f9c525805b9c44f13b491096e8949c54
primalkaze <44187697+primalkaze@users.noreply.github.com>
2021-06-27 13:19:19 +0530

    dmenu cleaned

--- dmenu/config.h
+++ dmenu/config.h
@@ -3,23 +3,23 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int centered = 0;                    /* -c option; centers dmenu on screen */
-static int min_width = 500;                    /* minimum width when centered */
+static int min_width = 200;                    /* minimum width when centered */
 /* -h option; minimum height of a menu line */
 static unsigned int lineheight = 8;
 static unsigned int min_lineheight = 8;
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"CaskaydiaCove Nerd Font:size=11"
+	"CaskaydiaCove Nerd Font:size=13"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#fff", "#222222" },
-	[SchemeSel] = { "#fff", "#98b379" },
+	[SchemeSel] = { "#fff", "#ABB2CF" },
 	[SchemeOut] = { "#000000", "#282c34" },
 	[SchemeMid] = { "#fff", "#282c34" },
-	[SchemeSelHighlight] = { "#000000", "#98c379" },
-	[SchemeNormHighlight] = { "#000000", "#98c379" },
+	[SchemeSelHighlight] = { "#000000", "#ABB2CF" },
+	[SchemeNormHighlight] = { "#000000", "#ABB2CF" },
 	[SchemeOutHighlight] = { "#000000", "#282c34" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
@@ -32,4 +32,4 @@ static unsigned int lines      = 0;
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static const unsigned int border_width = 2;
+static const unsigned int border_width = 1;
